98830b56345cbe291c4d460b292572b574a2ed61
Harshalszz <477harshz@gmail.com>
2026-02-12 21:22:19 +0530

    CellswithOddValuesMatrix Day249

---
+++ DSAProblems/src/ProblemsDay_249/CellswithOddValuesMatrix.java
@@ -0,0 +1,25 @@
+package ProblemsDay_249;
+
+public class CellswithOddValuesMatrix {
+
+    //https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/?envType=problem-list-v2&envId=array
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] row = new int[m], col = new int[n];
+        for (int[] arr : indices) {
+            row[arr[0]]++;
+            col[arr[1]]++;
+        }
+        int oddRow = 0, oddCol = 0;
+        for (int i = 0; i < m; i++)
+            if (row[i] % 2 == 1)
+                oddRow++;
+        for (int i = 0; i < n; i++)
+            if (col[i] % 2 == 1)
+                oddCol++;
+        return oddRow * (n - oddCol) + (m - oddRow) * oddCol;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
