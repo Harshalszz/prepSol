7a42708ab074634c742a1feceeda6132223e4ac2
Harshalszz <477harshz@gmail.com>
2026-02-13 21:27:00 +0530

    NumberPairsStringsWithConcatenationEquTarget Day250

---
+++ DSAProblems/src/ProblemsDay_250/NumberPairsStringsWithConcatenationEquTarget.java
@@ -0,0 +1,36 @@
+package ProblemsDay_250;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberPairsStringsWithConcatenationEquTarget {
+
+    //https://leetcode.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target/?envType=problem-list-v2&envId=string
+
+    public int numOfPairs(String[] nums, String target) {
+        Map<String,Integer> hm=new HashMap<>();
+        for(String s:nums){
+            hm.put(s,hm.getOrDefault(s,0)+1);
+        }
+        int pos=1,count=0;
+        int n=target.length();
+
+        while(pos<n){
+            String s1=target.substring(0,pos);
+            String s2=target.substring(pos,n);
+            if(hm.containsKey(s1)&&hm.containsKey(s2)){
+                if(s1.equals(s2)){
+                    count+=hm.get(s1)*(hm.get(s1)-1);
+                }else{
+                    count+=hm.get(s1)*hm.get(s2);
+                }
+
+            }
+            pos++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+
+    }
+}
