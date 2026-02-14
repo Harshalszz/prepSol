870883fc7d779af0f4ee08a6f1424345ca40336f
Harshalszz <477harshz@gmail.com>
2026-02-14 19:53:48 +0530

    CountServersthatCommunicate Day251

---
+++ DSAProblems/src/ProblemsDay_251/CountServersthatCommunicate.java
@@ -0,0 +1,39 @@
+package ProblemsDay_251;
+
+public class CountServersthatCommunicate {
+
+    //https://leetcode.com/problems/count-servers-that-communicate/?envType=problem-list-v2&envId=array
+    public int countServers(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] rowCnt=new int[m];
+        int[] colCnt=new int[n];
+
+        // Count servers in each row and column
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){
+                    rowCnt[i]++;
+                    colCnt[j]++;
+                }
+            }
+        }
+
+        int cnt=0;
+        // Count communicating servers
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){
+                    if(rowCnt[i]>1 || colCnt[j]>1){
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+    public static void main(String[] args) {
+
+    }
+}
