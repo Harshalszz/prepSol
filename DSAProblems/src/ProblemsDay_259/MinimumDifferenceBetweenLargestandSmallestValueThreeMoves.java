48c4e2d6d1ad968447a426f645f31bd1760f9846
Harshalszz <477harshz@gmail.com>
2026-02-22 23:52:34 +0530

    MinimumDifferenceBetweenLargestandSmallestValueThreeMoves Day259

---
+++ DSAProblems/src/ProblemsDay_259/MinimumDifferenceBetweenLargestandSmallestValueThreeMoves.java
@@ -0,0 +1,22 @@
+package ProblemsDay_259;
+
+import java.util.Arrays;
+
+public class MinimumDifferenceBetweenLargestandSmallestValueThreeMoves {
+
+//https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/?envType=problem-list-v2&envId=array
+    public int minDifference(int[] nums) {
+        if(nums.length <= 4) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int ans = nums[nums.length - 1] - nums[0];
+        for(int i = 0; i <= 3; i++) {
+            ans = Math.min(ans, nums[nums.length - (3 - i) - 1] - nums[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
