eef33f1c9df998ac81e461904c7b81bb5d4766ab
Harshalszz <477harshz@gmail.com>
2026-02-22 23:54:37 +0530

    CountGoodTriplets Day259

---
+++ DSAProblems/src/ProblemsDay_259/CountGoodTriplets.java
@@ -0,0 +1,33 @@
+package ProblemsDay_259;
+
+public class CountGoodTriplets {
+
+    //https://leetcode.com/problems/count-good-triplets/?envType=problem-list-v2&envId=array
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int res = 0;
+        int[] interval = new int[1001];  // Prefix count array
+
+        for (int j = 0; j < arr.length; j++) {
+            for (int k = j + 1; k < arr.length; k++) {
+                if (Math.abs(arr[j] - arr[k]) <= b) {
+                    int left = Math.max(0, Math.max(arr[j] - a, arr[k] - c));
+                    int right = Math.min(1000, Math.min(arr[j] + a, arr[k] + c));
+                    if (left <= right) {
+                        if (left == 0)
+                            res += interval[right];
+                        else
+                            res += interval[right] - interval[left - 1];
+                    }
+                }
+            }
+            for (int ind = arr[j]; ind <= 1000; ind++) {
+                interval[ind]++;
+            }
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
