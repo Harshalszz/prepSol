5da81414d4d6034ff400b0776e8e386ed9e88a34
Harshalszz <477harshz@gmail.com>
2026-02-28 21:59:44 +0530

    MakeTheStringGreat Day265

---
+++ DSAProblems/src/ProblemsDay_265/MakeTheStringGreat.java
@@ -0,0 +1,30 @@
+package ProblemsDay_265;
+
+public class MakeTheStringGreat {
+//https://leetcode.com/problems/make-the-string-great/?envType=problem-list-v2&envId=string
+    public String makeGood(String s) {
+        StringBuilder sb=new StringBuilder();
+        sb.append(s);
+        int flag=0;
+        while(flag==0&&sb.length()>0){
+            flag=1;
+            for(int i=0;i<sb.length()-1;i++){
+                if(isGreat(sb,i)){
+                    sb.delete(i,i+2);
+                    flag=0;
+                }
+            }
+        }
+        return sb.toString();
+    }
+    public static boolean isGreat(StringBuilder sb,int i){
+        if(Integer.valueOf(sb.charAt(i))==Integer.valueOf(sb.charAt(i+1))+32||Integer.valueOf(sb.charAt(i))==Integer.valueOf(sb.charAt(i+1))-32){
+
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
