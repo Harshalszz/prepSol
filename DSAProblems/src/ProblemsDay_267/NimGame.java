6d80fcface5d4af7cdf1dbacf129afea444f24e4
Harshalszz <477harshz@gmail.com>
2026-03-02 21:25:48 +0530

    NimGame Day267

---
+++ DSAProblems/src/ProblemsDay_267/NimGame.java
@@ -0,0 +1,12 @@
+package ProblemsDay_267;
+
+public class NimGame {
+
+    //https://leetcode.com/problems/nim-game/?envType=problem-list-v2&envId=math
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
