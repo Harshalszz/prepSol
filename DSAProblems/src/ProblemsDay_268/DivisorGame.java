3956c1e417e2cb339dda331bfbeafc50e5f81d74
Harshalszz <477harshz@gmail.com>
2026-03-03 20:31:57 +0530

    DivisorGame Day268

---
+++ DSAProblems/src/ProblemsDay_268/DivisorGame.java
@@ -0,0 +1,12 @@
+package ProblemsDay_268;
+
+public class DivisorGame {
+//https://leetcode.com/problems/divisor-game/?envType=problem-list-v2&envId=math
+
+    public boolean divisorGame(int n) {
+        return n%2==0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
