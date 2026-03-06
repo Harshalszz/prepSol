29fe3f13db61bf925f3ca5ebe1af860666f4d8a8
Harshalszz <477harshz@gmail.com>
2026-03-06 18:52:28 +0530

    MaximumNumberRemovableCharacters Day271

---
+++ DSAProblems/src/ProblemsDay_271/MaximumNumberRemovableCharacters.java
@@ -0,0 +1,33 @@
+package ProblemsDay_271;
+
+public class MaximumNumberRemovableCharacters {
+//https://leetcode.com/problems/maximum-number-of-removable-characters/?envType=problem-list-v2&envId=string
+
+    public boolean stillSubsequence(char[] letters, String p) {
+        int idx1=0, idx2=0, len1=letters.length, len2=p.length();
+        while(idx1++<len1 && idx2<len2)
+            if(letters[idx1-1]==p.charAt(idx2))
+                idx2++;
+        return idx2==len2;
+    }
+    public int maximumRemovals(String s, String p, int[] removable) {
+        char[] letters=s.toCharArray();
+        int left=0, right=removable.length;
+        while(left<=right) {
+            int mid=left+(right-left)/2;
+            for(int i=0;i<mid;i++)
+                letters[removable[i]]='*';
+            if(stillSubsequence(letters, p))
+                left=mid+1;
+            else {
+                for(int i=0;i<mid;i++)
+                    letters[removable[i]]=s.charAt(removable[i]);
+                right=mid-1;
+            }
+        }
+        return right;
+    }
+    public static void main(String[] args) {
+
+    }
+}
