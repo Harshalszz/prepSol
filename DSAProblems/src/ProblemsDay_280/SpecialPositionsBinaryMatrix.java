c577b8e1fb89fbed2e88ea2804f11bacc3e775f9
Harshalszz <477harshz@gmail.com>
2026-03-15 19:35:40 +0530

    SpecialPositionsBinaryMatrix Day280

---
+++ DSAProblems/src/ProblemsDay_280/SpecialPositionsBinaryMatrix.java
@@ -0,0 +1,32 @@
+package ProblemsDay_280;
+
+public class SpecialPositionsBinaryMatrix {
+//https://leetcode.com/problems/special-positions-in-a-binary-matrix/?envType=problem-list-v2&envId=array
+    public int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
