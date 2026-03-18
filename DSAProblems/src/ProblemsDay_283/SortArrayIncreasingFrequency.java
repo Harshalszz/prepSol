b2a898c4d768bac591fc85167d1a1a0d30fdf5d8
Harshalszz <477harshz@gmail.com>
2026-03-18 21:54:44 +0530

    SortArrayIncreasingFrequency Day283

---
+++ DSAProblems/src/ProblemsDay_283/SortArrayIncreasingFrequency.java
@@ -0,0 +1,25 @@
+package ProblemsDay_283;
+
+import java.util.Arrays;
+
+public class SortArrayIncreasingFrequency {
+
+//https://leetcode.com/problems/sort-array-by-increasing-frequency/?envType=problem-list-v2&envId=array
+    public int[] frequencySort(int[] nums) {
+        int[] count = new int[201];
+        for (int num : nums) {
+            count[num + 100]++;
+        }
+        Integer[] numsArr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(numsArr, (a, b) -> {
+            if (count[a + 100] == count[b + 100]) {
+                return b - a;
+            }
+            return count[a + 100] - count[b + 100];
+        });
+        return Arrays.stream(numsArr).mapToInt(Integer::intValue).toArray();
+    }
+    public static void main(String[] args) {
+
+    }
+}
