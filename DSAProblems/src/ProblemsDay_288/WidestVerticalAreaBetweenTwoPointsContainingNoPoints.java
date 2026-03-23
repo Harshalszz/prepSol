54ccad84edaaed0b9834c0368553e32473a928ff
Harshalszz <477harshz@gmail.com>
2026-03-23 22:08:59 +0530

    WidestVerticalAreaBetweenTwoPointsContainingNoPoints Day288

---
+++ DSAProblems/src/ProblemsDay_288/WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -0,0 +1,23 @@
+package ProblemsDay_288;
+
+import java.util.Arrays;
+
+public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
+//https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/?envType=problem-list-v2&envId=array
+
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int max_width = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            int width = points[i][0] - points[i - 1][0];
+            max_width = Math.max(max_width, width);
+        }
+
+        return max_width;
+    }
+    public static void main(String[] args) {
+
+    }
+}
