11503a85aede7bc924838b95354089f6c410e5aa
Harshalszz <477harshz@gmail.com>
2026-03-24 20:35:14 +0530

    SortthePeople Day289

---
+++ DSAProblems/src/ProblemsDay_289/SortthePeople.java
@@ -0,0 +1,28 @@
+package ProblemsDay_289;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SortthePeople {
+
+//https://leetcode.com/problems/sort-the-people/?envType=problem-list-v2&envId=string
+    public String[] sortPeople(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        String[] result = new String[heights.length];
+        int index = 0;
+        for (int i = heights.length - 1; i >= 0; i--) {
+            result[index] = map.get(heights[i]);
+            index++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
