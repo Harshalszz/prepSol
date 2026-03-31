5380706e2538454746f94e205a6d479ccab35946
Harshalszz <477harshz@gmail.com>
2026-03-31 20:36:11 +0530

    CheckifStringsCanbeMadeEqualWithOperations Day296

---
+++ DSAProblems/src/ProblemsDay_296/CheckifStringsCanbeMadeEqualWithOperations.java
@@ -0,0 +1,16 @@
+package ProblemsDay_296;
+
+public class CheckifStringsCanbeMadeEqualWithOperations {
+
+//https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/?envType=problem-list-v2&envId=string
+    public boolean canBeEqual(String s1, String s2) {
+        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+                ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                        (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
