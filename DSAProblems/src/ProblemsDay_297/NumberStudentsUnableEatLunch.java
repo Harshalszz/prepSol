9d5402ac3dc707ff13826ad7c3be01fab958ac10
Harshalszz <477harshz@gmail.com>
2026-04-01 20:46:54 +0530

    NumberStudentsUnableEatLunch Day297

--- DSAProblems/src/ProblemsDay_297/NumberStudentsUnableEatLunch.java
+++ DSAProblems/src/ProblemsDay_297/NumberStudentsUnableEatLunch.java
@@ -1,4 +1,18 @@
 package ProblemsDay_297;
 
 public class NumberStudentsUnableEatLunch {
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students) counts[student]++;
+
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) break;
+            if (remaining-- == 0) break;
+            counts[sandwich]--;
+        }
+
+        return remaining;
+    }
 }
