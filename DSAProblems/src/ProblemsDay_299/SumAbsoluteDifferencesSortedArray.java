b09bcdeffde2ade523e234770a5d2520387e59cc
Harshalszz <477harshz@gmail.com>
2026-04-03 11:52:54 +0530

    SumAbsoluteDifferencesSortedArray Day299

---
+++ DSAProblems/src/ProblemsDay_299/SumAbsoluteDifferencesSortedArray.java
@@ -0,0 +1,30 @@
+package ProblemsDay_299;
+
+public class SumAbsoluteDifferencesSortedArray {
+//https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/?envType=problem-list-v2&envId=array
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        int left = 0;
+        int right = sum;
+
+        int[] r = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            right -= n;
+
+            r[i] = n * i - left + right - n * (nums.length - i - 1);
+
+            left += n;
+        }
+
+        return r;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
