d93c07a467dd2890088e66ab1606f0aec0896524
Harshalszz <477harshz@gmail.com>
2026-04-13 23:10:41 +0530

    CountVowelStringsRanges Day309

---
+++ DSAProblems/src/ProblemsDay_309/CountVowelStringsRanges.java
@@ -0,0 +1,33 @@
+package ProblemsDay_309;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountVowelStringsRanges
+{
+//https://leetcode.com/problems/count-vowel-strings-in-ranges/?envType=problem-list-v2&envId=string
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        int[] Prefix = new int[n + 1];
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+        for (int i = 0; i < n; i++) {
+            Prefix[i + 1] = Prefix[i];
+            if (vowels.contains(words[i].charAt(0)) && vowels.contains(words[i].charAt(words[i].length() - 1))) {
+                Prefix[i + 1]++;
+            }
+        }
+
+        int[] ANS = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ANS[i] = Prefix[queries[i][1] + 1] - Prefix[queries[i][0]];
+        }
+        return ANS;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
