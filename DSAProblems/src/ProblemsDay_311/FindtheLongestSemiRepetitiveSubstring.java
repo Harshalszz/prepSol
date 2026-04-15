08c73351a32cb696c4e9763f4da3f45a4ad0ad83
Harshalszz <477harshz@gmail.com>
2026-04-15 21:25:31 +0530

    FindtheLongestSemiRepetitiveSubstring Day311

---
+++ DSAProblems/src/ProblemsDay_311/FindtheLongestSemiRepetitiveSubstring.java
@@ -0,0 +1,19 @@
+package ProblemsDay_311;
+
+public class FindtheLongestSemiRepetitiveSubstring
+{
+//https://leetcode.com/problems/find-the-longest-semi-repetitive-substring/?envType=problem-list-v2&envId=string
+    public int longestSemiRepetitiveSubstring(String s) {
+        int i = 0, cur = 0, n = s.length();
+        for (int j = 1; j < n; ++j) {
+            cur += s.charAt(j) == s.charAt(j - 1) ? 1 : 0;
+            if (cur > 1) {
+                cur -= s.charAt(++i) == s.charAt(i - 1) ? 1 : 0;
+            }
+        }
+        return n - i;
+    }
+    public static void main(String[] args) {
+
+    }
+}
