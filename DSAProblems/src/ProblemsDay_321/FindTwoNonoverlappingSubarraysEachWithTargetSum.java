9093bb61edf3daf07a2a95d1a6eecb3c2fda80dc
Harshalszz <477harshz@gmail.com>
2026-04-28 21:51:43 +0530

    FindTwoNonoverlappingSubarraysEachWithTargetSum Day321

---
+++ DSAProblems/src/ProblemsDay_321/FindTwoNonoverlappingSubarraysEachWithTargetSum.java
@@ -0,0 +1,39 @@
+package ProblemsDay_321;
+
+import java.util.Arrays;
+
+public class FindTwoNonoverlappingSubarraysEachWithTargetSum {
+//https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/?envType=problem-list-v2&envId=array
+
+    public int minSumOfLengths(int[] arr, int target) {
+        int[] min_len = new int[arr.length];
+        Arrays.fill(min_len, Integer.MAX_VALUE);
+
+        int sum = 0;
+        int start = 0;
+        int res = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int end = 0; end < arr.length; end++){
+            sum += arr[end];
+
+            while(sum > target){
+                sum -= arr[start++];
+            }
+
+            if(sum == target){
+                int cur_len = end-start+1;
+                if(start > 0 && min_len[start-1] != Integer.MAX_VALUE){
+                    res = Math.min(res, cur_len + min_len[start-1]);
+                }
+                min = Math.min(min, cur_len);
+            }
+            min_len[end] = min;
+        }
+
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
