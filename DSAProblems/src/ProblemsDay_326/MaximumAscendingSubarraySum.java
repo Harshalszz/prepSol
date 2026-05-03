6939c327bdd60a9bcd33a7e901d7d1fd37e0124a
Harshalszz <477harshz@gmail.com>
2026-05-03 18:50:14 +0530

    MaximumAscendingSubarraySum Day326

---
+++ DSAProblems/src/ProblemsDay_326/MaximumAscendingSubarraySum.java
@@ -0,0 +1,24 @@
+package ProblemsDay_326;
+
+public class MaximumAscendingSubarraySum {
+//https://leetcode.com/problems/maximum-ascending-subarray-sum/?envType=problem-list-v2&envId=array
+
+    public int maxAscendingSum(int[] nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+    public static void main(String[] args) {
+
+    }
+}
