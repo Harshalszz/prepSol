8161892c3820c5db969226c832ff8788df00d934
Harshalszz <477harshz@gmail.com>
2026-05-12 22:05:21 +0530

    SeparateBlackandWhiteBalls Day335

---
+++ DSAProblems/src/ProblemsDay_335/SeparateBlackandWhiteBalls.java
@@ -0,0 +1,23 @@
+package ProblemsDay_335;
+
+public class SeparateBlackandWhiteBalls {
+
+    public long minimumSteps(String s) {
+        long ans = 0;
+        long zeroCnt = 0;
+
+        // Looping through the string in reverse order
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);  // Getting the character at position i
+            if (ch == '0') {
+                zeroCnt++;
+            } else if (ch == '1') {
+                ans += zeroCnt;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
