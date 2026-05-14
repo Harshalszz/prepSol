7e6a7e24444ad6d32c93c3eed385ddf76442c362
Harshalszz <477harshz@gmail.com>
2026-05-14 22:02:59 +0530

    WeightedWordMapping Day337

--- DSAProblems/src/ProblemsDay_337/WeightedWordMapping.java
+++ DSAProblems/src/ProblemsDay_337/WeightedWordMapping.java
@@ -2,7 +2,19 @@ package ProblemsDay_337;
 
 public class WeightedWordMapping {
 
+//https://leetcode.com/problems/weighted-word-mapping/?envType=problem-list-v2&envId=string
+    public String mapWordWeights(String[] words, int[] weights) {
 
+        StringBuilder ans = new StringBuilder();
+        for (String s : words) {
+            int cnt = 0;
+            for (int i = 0; i < s.length(); i++) {
+                cnt += weights[s.charAt(i) - 'a'];
+            }
+            ans.append((char)('a' + (25 - (cnt % 26))));
+        }
+        return ans.toString();
+    }
     public static void main(String[] args) {
 
     }
