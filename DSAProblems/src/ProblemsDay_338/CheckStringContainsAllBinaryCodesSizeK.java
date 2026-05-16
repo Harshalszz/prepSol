1fd634fb262019971c44d74b28464c11d389ebab
Harshalszz <477harshz@gmail.com>
2026-05-16 19:15:58 +0530

    CheckStringContainsAllBinaryCodesSizeK Day338

---
+++ DSAProblems/src/ProblemsDay_338/CheckStringContainsAllBinaryCodesSizeK.java
@@ -0,0 +1,27 @@
+package ProblemsDay_338;
+
+public class CheckStringContainsAllBinaryCodesSizeK {
+
+//https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/?envType=problem-list-v2&envId=string
+    public boolean hasAllCodes(String s, int k) {
+        int req = 1 << k;
+        boolean[] seen = new boolean[req];
+        int mask = req - 1;
+        int hash = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            hash = ((hash << 1) & mask) | (s.charAt(i) & 1);
+
+            if (i >= k - 1 && !seen[hash]) {
+                seen[hash] = true;
+                req--;
+                if (req == 0) return true;
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
