b03cb7989a55562c652f1f9555e0d5205f698d5b
Harshalszz <477harshz@gmail.com>
2026-05-18 21:30:05 +0530

    WordLadder Day340

---
+++ DSAProblems/src/ProblemsDay_340/WordLadder.java
@@ -0,0 +1,56 @@
+package ProblemsDay_340;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordLadder {
+
+//https://leetcode.com/problems/word-ladder/?envType=problem-list-v2&envId=string
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
+
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        int steps = 1;
+
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+
+            Set<String> nextSet = new HashSet<>();
+
+            for (String word : beginSet) {
+                for (int i = 0; i < word.length(); i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == word.charAt(i)) continue;
+
+                        String newWord = word.substring(0, i) + c + word.substring(i + 1);
+                        if (endSet.contains(newWord)) return steps + 1;
+
+                        if (wordSet.contains(newWord) && !visited.contains(newWord)) {
+                            visited.add(newWord);
+                            nextSet.add(newWord);
+                        }
+                    }
+                }
+            }
+
+            beginSet = nextSet;
+            steps++;
+        }
+
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
