57a899f64a367642303d3fda0b399359997325f0
Harshalszz <477harshz@gmail.com>
2026-05-28 18:59:32 +0530

    ApplyDiscountEveryOrders Day350

---
+++ DSAProblems/src/ProblemsDay_350/ApplyDiscountEveryOrders.java
@@ -0,0 +1,38 @@
+package ProblemsDay_350;
+
+public class ApplyDiscountEveryOrders {
+
+//https://leetcode.com/problems/apply-discount-every-n-orders/?envType=problem-list-v2&envId=array
+    class Cashier {
+        int discount;
+        int n;
+        int []products;
+        int []prices=new int[200];
+        public Cashier(int n, int discount, int[] products, int[] prices) {
+            this.discount=discount;
+            this.n=n;
+            this.products=products;
+            for(int i=0; i<products.length; i++){
+                this.prices[products[i]-1]=prices[i];
+            }
+        }
+        int count=1;
+        public double getBill(int[] product, int[] amount) {
+            double cost=0;
+            for(int i=0; i<product.length; i++){
+                cost+=amount[i]*this.prices[product[i]-1];
+            }
+            if(count==n){
+                cost*=(double)(100-this.discount)/100.00;
+                count=1;
+            }else{
+                count++;
+            }
+            return cost;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
