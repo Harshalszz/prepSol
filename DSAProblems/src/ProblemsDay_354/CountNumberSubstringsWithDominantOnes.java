17286a70d127d5ca7c9a7827f899f72a52c4011b
Harshalszz <477harshz@gmail.com>
2026-06-01 22:40:43 +0530

    CountNumberSubstringsWithDominantOnes Day354

---
+++ DSAProblems/src/ProblemsDay_354/CountNumberSubstringsWithDominantOnes.java
@@ -0,0 +1,35 @@
+package ProblemsDay_354;
+
+public class CountNumberSubstringsWithDominantOnes {
+
+//https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/?envType=problem-list-v2&envId=string
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int[] pre = new int[n + 1];
+        pre[0] = -1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || (i > 0 && s.charAt(i - 1) == '0')) {
+                pre[i + 1] = i;
+            } else {
+                pre[i + 1] = pre[i];
+            }
+        }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt0 = s.charAt(i - 1) == '0' ? 1 : 0;
+            int j = i;
+            while (j > 0 && cnt0 * cnt0 <= n) {
+                int cnt1 = (i - pre[j]) - cnt0;
+                if (cnt0 * cnt0 <= cnt1) {
+                    res += Math.min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
+                }
+                j = pre[j];
+                cnt0++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
