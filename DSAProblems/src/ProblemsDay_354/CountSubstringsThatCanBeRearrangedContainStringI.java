e957a5e38f4ed3743caa910f244ee3d5b551f1d8
Harshalszz <477harshz@gmail.com>
2026-06-01 20:11:01 +0530

    CountSubstringsThatCanBeRearrangedContainStringI Day354

---
+++ DSAProblems/src/ProblemsDay_354/CountSubstringsThatCanBeRearrangedContainStringI.java
@@ -0,0 +1,9 @@
+package ProblemsDay_354;
+
+public class CountSubstringsThatCanBeRearrangedContainStringI {
+//https://leetcode.com/problems/count-substrings-that-can-be-rearranged-to-contain-a-string-i/description/?envType=problem-list-v2&envId=string
+
+    public static void main(String[] args) {
+
+    }
+}
