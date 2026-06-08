b3471a027af833c80d4fa9e283bd449c5e60af2c
Harshalszz <477harshz@gmail.com>
2026-06-08 21:27:23 +0530

    RemoveCoveredIntervals Day361

---
+++ DSAProblems/src/ProblemsDay_361/RemoveCoveredIntervals.java
@@ -0,0 +1,38 @@
+package ProblemsDay_361;
+
+import java.util.Arrays;
+
+public class RemoveCoveredIntervals {
+
+//https://leetcode.com/problems/remove-covered-intervals/?envType=problem-list-v2&envId=array
+
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a, b)->{
+            if(a[0]==b[0]){
+                return Integer.compare(b[1],a[1]);
+            }
+            else{
+                return Integer.compare(a[0],b[0]);
+            }
+        });
+
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        int removed = 0;
+
+        for(int i=1; i<intervals.length; i++){
+            if(l <= intervals[i][0] && intervals[i][1] <= r){
+                removed++; // interval covered
+            }
+            else{
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }
+        }
+        return intervals.length - removed;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
