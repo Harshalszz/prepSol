75046fe04466625d39223a2c670c5f12afb80713
Harshalszz <477harshz@gmail.com>
2026-06-11 20:27:44 +0530

    CountEqualDivisiblePairsArray Day363

---
+++ DSAProblems/src/ProblemsDay_363/CountEqualDivisiblePairsArray.java
@@ -0,0 +1,37 @@
+package ProblemsDay_363;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountEqualDivisiblePairsArray
+{
+//https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/?envType=problem-list-v2&envId=array
+    public int countPairs(int[] nums, int k) {
+        Map<Integer, Map<Integer, Integer>> moduloMap = new HashMap<>();
+        int res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int mod_i = i % k;
+            int num = nums[i];
+
+            if (moduloMap.containsKey(num)) {
+                for (Map.Entry<Integer, Integer> entry : moduloMap.get(num).entrySet()) {
+                    int mod_j = entry.getKey();
+                    int count = entry.getValue();
+                    if ((mod_i * mod_j) % k == 0) {
+                        res += count;
+                    }
+                }
+            }
+
+            moduloMap.putIfAbsent(num, new HashMap<>());
+            Map<Integer, Integer> innerMap = moduloMap.get(num);
+            innerMap.put(mod_i, innerMap.getOrDefault(mod_i, 0) + 1);
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
