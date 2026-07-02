0de61630278ad9bb5f900c4074f84c5593340549
Harshalszz <477harshz@gmail.com>
2026-07-02 21:33:19 +0530

    AverageWaitingTime Day381

---
+++ DSAProblems/src/ProblemsDay_381/AverageWaitingTime.java
@@ -0,0 +1,27 @@
+package ProblemsDay_381;
+
+public class AverageWaitingTime {
+//https://leetcode.com/problems/average-waiting-time/?envType=problem-list-v2&envId=array
+
+    public double averageWaitingTime(int[][] customers) {
+        double total_waiting_time = 0;
+        int current_time = 0;
+
+        for (int[] customer : customers) {
+            int arrival = customer[0];
+            int service = customer[1];
+            if (current_time < arrival) {
+                current_time = arrival;
+            }
+            int waiting_time = current_time - arrival + service;
+            total_waiting_time += waiting_time;
+            current_time += service;
+        }
+
+        return total_waiting_time / customers.length;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
