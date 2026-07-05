3c08575646843a474a507823a469485e6e29898c
Harshalszz <477harshz@gmail.com>
2026-07-05 20:33:28 +0530

    RemoveDuplicatesSortedArrayII Day383

---
+++ DSAProblems/src/ProblemsDay_383/RemoveDuplicatesSortedArrayII.java
@@ -0,0 +1,24 @@
+package ProblemsDay_383;
+
+public class RemoveDuplicatesSortedArrayII {
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+    public int removeDuplicates(int[] nums) {
+
+        int k = 2;
+
+        for(int i=2; i< nums.length; i++){
+
+            if(nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
