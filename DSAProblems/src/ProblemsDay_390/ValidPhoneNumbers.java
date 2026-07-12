29b66ddb4769125d7921944bf75cfcaea6471866
Harshalszz <477harshz@gmail.com>
2026-07-12 20:27:26 +0530

    ValidPhoneNumbers Day390

---
+++ DSAProblems/src/ProblemsDay_390/ValidPhoneNumbers.java
@@ -0,0 +1,9 @@
+package ProblemsDay_390;
+
+public class ValidPhoneNumbers {
+    //grep -E '^((\([0-9]{3}\) [0-9]{3}-[0-9]{4})|([0-9]{3}-[0-9]{3}-[0-9]{4}))$' file.txt
+
+    public static void main(String[] args) {
+
+    }
+}
