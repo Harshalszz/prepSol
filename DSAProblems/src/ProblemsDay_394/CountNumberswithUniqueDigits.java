dac8f83aa89968af623577392cacef8a5ad27719
Harshalszz <477harshz@gmail.com>
2026-07-16 21:44:42 +0530

    CountNumberswithUniqueDigits Day394

---
+++ DSAProblems/src/ProblemsDay_394/CountNumberswithUniqueDigits.java
@@ -0,0 +1,22 @@
+package ProblemsDay_394;
+
+public class CountNumberswithUniqueDigits {
+//https://leetcode.com/problems/count-numbers-with-unique-digits/
+
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int total = 10;
+        int prod = 9;
+        for (int i = 2; i < n + 1; i++) {
+            total += prod * (11 - i);
+            prod *= 11 - i;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
