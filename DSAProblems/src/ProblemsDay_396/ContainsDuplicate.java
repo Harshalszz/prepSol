bcc38bf610b67b38b1538267fef271d7b21a40e7
Harshalszz <477harshz@gmail.com>
2026-07-18 14:45:58 +0530

    ContainsDuplicate Day396

---
+++ DSAProblems/src/ProblemsDay_396/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+package ProblemsDay_396;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        return Arrays.stream(nums).distinct().count() < nums.length;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
