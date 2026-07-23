ec5e8aee86d185cda2ff85212df3704e985c7e79
Harshalszz <477harshz@gmail.com>
2026-07-23 19:56:53 +0530

    ContainsDuplicateII Day401

---
+++ DSAProblems/src/ProblemsDay_401/ContainsDuplicateII.java
@@ -0,0 +1,51 @@
+package ProblemsDay_401;
+
+import java.util.HashMap;
+
+public class ContainsDuplicateII {
+
+    //https://leetcode.com/problems/contains-duplicate-ii/
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        //  Set<Integer> seen = new HashSet<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i > k) {
+        //         seen.remove(nums[i - k - 1]);
+        //     }
+
+        //     if (seen.contains(nums[i])) {
+        //         return true;
+        //     }
+
+        //     seen.add(nums[i]);
+        // }
+
+        // return false;
+
+        HashMap<Integer, Integer> indxMap = new HashMap<>();
+
+        for(int i =0 ; i<nums.length;i++){
+            if(indxMap.containsKey(nums[i])){
+                int index = indxMap.get(nums[i]);
+
+                if((i-index) <=k){
+                    return true;
+                }else{
+                    indxMap.put(nums[i] ,i);
+                }
+
+            }else{
+                indxMap.put(nums[i] ,i);
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
