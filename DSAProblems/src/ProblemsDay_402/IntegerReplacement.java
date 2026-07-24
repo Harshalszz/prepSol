9604e4880f57866209bd8cc42f09775f4f8536e8
Harshalszz <477harshz@gmail.com>
2026-07-24 20:04:42 +0530

    IntegerReplacement Day402

---
+++ DSAProblems/src/ProblemsDay_402/IntegerReplacement.java
@@ -0,0 +1,27 @@
+package ProblemsDay_402;
+
+public class IntegerReplacement {
+
+
+    public int integerReplacement(int n) {
+        long temp = n ;
+        int c = 0;
+        while(temp !=1){
+            if(temp%2==0){
+                temp/=2;
+            }
+            else if(temp==3 || temp%4==1){
+                temp -=1;
+            }
+            else{
+                temp+=1;
+            }
+            c++;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
