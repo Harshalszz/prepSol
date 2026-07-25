6416388b467aceeedcdbaa625fecf7cc0d61118f
Harshalszz <477harshz@gmail.com>
2026-07-25 19:26:17 +0530

    SubsetsII Day403

---
+++ DSAProblems/src/ProblemsDay_403/SubsetsII.java
@@ -0,0 +1,37 @@
+package ProblemsDay_403;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+
+//https://leetcode.com/problems/subsets-ii/
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(0, nums, subset, res);
+        return res;
+    }
+
+    private void backtrack(int i, int[] nums, List<Integer> subset, List<List<Integer>> res) {
+        if (i == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        backtrack(i + 1, nums, subset, res);
+        subset.remove(subset.size() - 1);
+
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+
+        backtrack(i + 1, nums, subset, res);
+    }
+    public static void main(String[] args) {
+
+    }
+}
