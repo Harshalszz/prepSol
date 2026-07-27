36099e77bcbc0832542f6000418955f7a8d00e98
Harshalszz <477harshz@gmail.com>
2026-07-27 20:35:57 +0530

    NimGame Day405

---
+++ DSAProblems/src/ProblemsDay_405/NimGame.java
@@ -0,0 +1,11 @@
+package ProblemsDay_405;
+
+public class NimGame {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
