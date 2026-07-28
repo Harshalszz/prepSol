1c251cdb1a7cd36c9faf784ff4325eb2383682bb
Harshalszz <477harshz@gmail.com>
2026-07-28 22:01:10 +0530

    FindAllDuplicatesArray Day406

---
+++ DSAProblems/src/ProblemsDay_406/FindAllDuplicatesArray.java
@@ -0,0 +1,25 @@
+package ProblemsDay_406;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicatesArray {
+
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                ans.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
