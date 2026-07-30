dd39eacaf3fe2d120dcc04dc4bbbb46df513362b
Harshalszz <477harshz@gmail.com>
2026-07-30 20:23:55 +0530

    PairsSongsWithTotalDurationsDivisible60 Day408

---
+++ DSAProblems/src/ProblemsDay_408/PairsSongsWithTotalDurationsDivisible60.java
@@ -0,0 +1,27 @@
+package ProblemsDay_408;
+
+import java.util.HashMap;
+
+public class PairsSongsWithTotalDurationsDivisible60 {
+
+    //https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+    public int numPairsDivisibleBy60(int[] time) {
+        int n = time.length, ans = 0;
+        HashMap<Integer, Integer> len = new HashMap<>();
+        for(int i  = 0; i < n; i++){
+            int rem = time[i] % 60;
+            int target = 60 - (rem);
+            if(len.containsKey(target))
+                ans += len.get(target);
+            if(rem != 0)
+                len.put(rem, len.getOrDefault(rem, 0) + 1);
+            else
+                len.put(60, len.getOrDefault(60, 0) + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
