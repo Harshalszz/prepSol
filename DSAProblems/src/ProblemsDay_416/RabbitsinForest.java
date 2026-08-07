18c432f60cf8c5a249bff1a0f0f37d99b576e20c
Harshalszz <477harshz@gmail.com>
2026-08-07 22:12:11 +0530

    RabbitsinForest Day416

---
+++ DSAProblems/src/ProblemsDay_416/RabbitsinForest.java
@@ -0,0 +1,30 @@
+package ProblemsDay_416;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RabbitsinForest {
+
+//https://leetcode.com/problems/rabbits-in-forest/?envType=problem-list-v2&envId=hash-table
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int a : answers) {
+            freq.put(a, freq.getOrDefault(a, 0) + 1);
+        }
+
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int k = entry.getKey();
+            int v = entry.getValue();
+            int groupSize = k + 1;
+            int groups = (v + k) / groupSize;
+            count += groups * groupSize;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+
+    }
+}
