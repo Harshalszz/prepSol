cdb562e9f57af44dd128b73f9eebf931dbf50302
Harshalszz <477harshz@gmail.com>
2026-08-07 22:10:17 +0530

    FairCandySwap Day416

---
+++ DSAProblems/src/ProblemsDay_416/FairCandySwap.java
@@ -0,0 +1,30 @@
+package ProblemsDay_416;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FairCandySwap {
+
+//https://leetcode.com/problems/fair-candy-swap/?envType=problem-list-v2&envId=hash-table
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumAlice = 0, sumBob = 0;
+        for (int a : aliceSizes) sumAlice += a;
+        for (int b : bobSizes) sumBob += b;
+        int delta = (sumBob - sumAlice) / 2;
+        Set<Integer> bobSet = new HashSet<>();
+        for (int b : bobSizes) {
+            bobSet.add(b);
+        }
+        for (int a : aliceSizes) {
+            int neededBobCandy = a + delta;
+            if (bobSet.contains(neededBobCandy)) {
+                return new int[]{a, neededBobCandy};
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
