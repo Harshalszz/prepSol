4ec8292846a3f3dbe61b5be9e54e9003d64d5ff2
Harshalszz <477harshz@gmail.com>
2026-08-13 21:46:32 +0530

    ContinuousSubarrays Day422

---
+++ DSAProblems/src/ProblemsDay_422/ContinuousSubarrays.java
@@ -0,0 +1,44 @@
+package ProblemsDay_422;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ContinuousSubarrays {
+
+//https://leetcode.com/problems/continuous-subarrays/?envType=problem-list-v2&envId=monotonic-queue
+    public long continuousSubarrays(int[] nums) {
+        int l = 0;
+        long res = 0;  // Use long to avoid overflow
+        Deque<Integer> minD = new LinkedList<>(), maxD = new LinkedList<>();
+
+        for (int r = 0; r < nums.length; r++) {
+            // Maintain the min deque
+            while (!minD.isEmpty() && nums[minD.peekLast()] >= nums[r]) {
+                minD.pollLast();
+            }
+            // Maintain the max deque
+            while (!maxD.isEmpty() && nums[maxD.peekLast()] <= nums[r]) {
+                maxD.pollLast();
+            }
+
+            // Add the current element index to the deques
+            minD.offerLast(r);
+            maxD.offerLast(r);
+
+            // Shrink the window from the left until the condition holds
+            while (nums[maxD.peekFirst()] - nums[minD.peekFirst()] > 2) {
+                l++;
+                if (minD.peekFirst() < l) minD.pollFirst();
+                if (maxD.peekFirst() < l) maxD.pollFirst();
+            }
+
+            // Add the valid subarrays count from l to r
+            res += r - l + 1;
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
