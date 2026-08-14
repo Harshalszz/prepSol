fdb107227adda322e724bc047217a473be7346fe
Harshalszz <477harshz@gmail.com>
2026-08-14 22:40:35 +0530

    ReverseLinkedList Day423

---
+++ DSAProblems/src/ProblemsDay_423/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package ProblemsDay_423;
+
+import ProblemsDay2.ListNode;
+
+public class ReverseLinkedList {
+
+//https://leetcode.com/problems/reverse-linked-list/?envType=problem-list-v2&envId=linked-list
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev=null, curr=head;
+
+        while(curr !=null){
+
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev= curr;
+            curr = nextNode;
+        }
+
+        return prev;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
