44e03589d5ba621b9b712d71052c5570327041f4
Harshalszz <477harshz@gmail.com>
2026-08-16 21:11:13 +0530

    MajorityElement Day425

---
+++ DSAProblems/src/ProblemsDay_425/MajorityElement.java
@@ -0,0 +1,30 @@
+package ProblemsDay_425;
+
+public class MajorityElement {
+//https://leetcode.com/problems/majority-element/?envType=problem-list-v2&envId=sorting
+    public int majorityElement(int[] nums) {
+
+        int ans = nums[0];
+        int count = 1;
+
+        for(int i=1; i<nums.length; i++){
+
+            if(nums[i] ==ans){
+                count++;
+            }else{
+                count--;
+            }
+
+            if(count == 0){
+                ans = nums[i];
+                count = 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
