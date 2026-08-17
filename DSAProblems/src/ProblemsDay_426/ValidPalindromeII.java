331f063b11478c7983a1dfb4dec8eb5ad11dbb20
Harshalszz <477harshz@gmail.com>
2026-08-17 20:54:12 +0530

    ValidPalindromeII Day426

---
+++ DSAProblems/src/ProblemsDay_426/ValidPalindromeII.java
@@ -0,0 +1,32 @@
+package ProblemsDay_426;
+
+public class ValidPalindromeII {
+
+//https://leetcode.com/problems/valid-palindrome-ii/?envType=problem-list-v2&envId=greedy
+    public boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
