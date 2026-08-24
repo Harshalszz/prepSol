cbbfbfb147cf9e3f0400e9b1a733d965059bb609
Harshalszz <477harshz@gmail.com>
2026-08-24 21:00:36 +0530

    ValidAnagram Day433

---
+++ DSAProblems/src/ProblemsDay_433/ValidAnagram.java
@@ -0,0 +1,31 @@
+package ProblemsDay_433;
+
+public class ValidAnagram {
+
+//https://leetcode.com/problems/valid-anagram/?envType=problem-list-v2&envId=string
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] character = new int[26];
+
+        for(char ch : s.toCharArray()){
+            character[ch-'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            if(character[ch-'a'] == 0){
+                return false;
+            }
+            character[ch-'a']--;
+        }
+
+        return true;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
