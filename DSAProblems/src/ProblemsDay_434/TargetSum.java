7d9bf532ad90aebb220cedae19401d9352912be6
Harshalszz <477harshz@gmail.com>
2026-08-25 21:04:41 +0530

    TargetSum Day434

---
+++ DSAProblems/src/ProblemsDay_434/TargetSum.java
@@ -0,0 +1,22 @@
+package ProblemsDay_434;
+
+public class TargetSum {
+
+//https://leetcode.com/problems/target-sum/?envType=problem-list-v2&envId=backtracking
+    public int findTargetSumWays(int[] nums, int target) {
+        return helper(nums, target, 0, 0);
+    }
+
+    private int helper(int[] arr, int target, int idx, int sum) {
+        if (idx >= arr.length)
+            return sum == target ? 1 : 0;
+
+        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
+        int add = helper(arr, target, idx + 1, sum + arr[idx]);
+
+        return subtract + add;
+    }
+    public static void main(String[] args) {
+
+    }
+}
