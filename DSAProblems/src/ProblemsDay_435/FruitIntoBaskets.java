c8e7af3afc456969c55ab6c67a61e49ce7264b9b
Harshalszz <477harshz@gmail.com>
2026-08-26 22:31:54 +0530

    FruitIntoBasket Day435

--- DSAProblems/src/ProblemsDay_435/FruitIntoBaskets.java
+++ DSAProblems/src/ProblemsDay_435/FruitIntoBaskets.java
@@ -7,29 +7,6 @@ public class FruitIntoBaskets {
 //https://leetcode.com/problems/fruit-into-baskets/?envType=problem-list-v2&envId=sliding-window
     public int totalFruit(int[] fruits) {
 
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int l=0, r=0, maxlen = 0;
-
-        // while(r<fruits.length){
-        //     map.put(fruits[r] , map.getOrDefault(fruits[r],0)+1);
-
-        //     while(map.size() > 2){
-        //         map.put(fruits[l] , map.get(fruits[l] ) - 1);
-        //         if(map.get(fruits[l]) == 0){
-        //             map.remove(fruits[l]);
-        //         }
-
-        //         l++;
-        //     }
-
-        //     if(map.size() <=2){
-        //         maxlen = Math.max(maxlen, r-l+1);
-        //     }
-
-        //     r++;
-        // }
-
-        // return maxlen;
 
         HashMap<Integer,Integer> map = new HashMap<>();
 
