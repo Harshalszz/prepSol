de10afb5a0e4afdd7016ceb980247894252a2d62
Harshalszz <477harshz@gmail.com>
2026-09-04 19:00:26 +0530

    ConvertSortedListBinarySearchTree Day440

---
+++ DSAProblems/src/ProblemsDay_440/ConvertSortedListBinarySearchTree.java
@@ -0,0 +1,39 @@
+package ProblemsDay_440;
+
+import ProblemsDay2.ListNode;
+import ProblemsDay_25.TreeNode;
+
+public class ConvertSortedListBinarySearchTree {
+
+
+    public TreeNode sortedListToBST(ListNode head) {
+
+        if(head == null) {return null;}
+
+        if(head.next == null){ return new TreeNode(head.val);}
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode mid = slow;
+
+        while(fast !=null && fast.next !=null){
+            mid = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        TreeNode node = new TreeNode(slow.val);
+
+        mid.next = null;
+
+        node.left = sortedListToBST(head);
+        node.right = sortedListToBST(slow.next);
+
+        return node;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
