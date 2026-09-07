287172aa391cafcfb31ded14feadda66e14afabb
Harshalszz <477harshz@gmail.com>
2026-09-07 21:22:10 +0530

    CountSubarraysWithMajorityElement Day443

---
+++ DSAProblems/src/ProblemsDay_443/CountSubarraysWithMajorityElement.java
@@ -0,0 +1,25 @@
+package ProblemsDay_443;
+
+public class CountSubarraysWithMajorityElement {
+
+
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int res = 0;
+
+        for(int i=0;i<nums.length;i++){
+            int count = 0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[j] == target)count++;
+
+                if((j-i+1)/2 < count){
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
