9532f09a906384f6cc3b0e0c1d04db8f09ab8e9a
Harshalszz <477harshz@gmail.com>
2026-09-13 19:07:16 +0530

    FinalPricesWithSpecialDiscountShop Day447

---
+++ DSAProblems/src/ProblemsDay_447/FinalPricesWithSpecialDiscountShop.java
@@ -0,0 +1,26 @@
+package ProblemsDay_447;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class FinalPricesWithSpecialDiscountShop {
+
+
+    public int[] finalPrices(int[] prices) {
+        int[] ans = prices.clone();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int j = 0; j < prices.length; ++j) {
+            // stack[-1] := i in the problem description.
+            while (!stack.isEmpty() && prices[j] <= prices[stack.peek()])
+                ans[stack.pop()] -= prices[j];
+            stack.push(j);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
