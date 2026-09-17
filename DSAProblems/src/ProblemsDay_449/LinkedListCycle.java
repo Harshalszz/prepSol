c95eb02bcd66428db597c1fae9aff6c121b29773
Harshalszz <477harshz@gmail.com>
2026-09-17 21:04:07 +0530

    LinkedListCycle Day449

---
+++ DSAProblems/src/ProblemsDay_449/LinkedListCycle.java
@@ -0,0 +1,25 @@
+package ProblemsDay_449;
+
+import ProblemsDay2.ListNode;
+
+public class LinkedListCycle {
+
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head, fast =head;
+
+        while(fast !=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
