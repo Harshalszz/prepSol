564391a9811931a96db97fc0712e3649a55117a3
Harshalszz <477harshz@gmail.com>
2026-09-20 19:51:34 +0530

    BestTimetoBuyandSellStockII Day451

---
+++ DSAProblems/src/ProblemsDay_451/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,18 @@
+package ProblemsDay_451;
+
+public class BestTimetoBuyandSellStockII {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0 ;
+
+        for(int i=1 ; i<prices.length ; i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit ;
+    }
+    public static void main(String[] args) {
+
+    }
+}
