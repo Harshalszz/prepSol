4b875edb3c4d4c81324d76dc57777dd42325717b
Harshalszz <477harshz@gmail.com>
2026-09-20 19:50:42 +0530

    SingleNumber Day451

---
+++ DSAProblems/src/ProblemsDay_451/SingleNumber.java
@@ -0,0 +1,30 @@
+package ProblemsDay_451;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+
+        // if(nums.length == 1){
+        //     return nums[0];
+        // }
+
+        // int res = nums[0];
+
+        // for(int i=1; i<nums.length; i++){
+        //     res = res^nums[i];
+        // }
+
+        // return res;
+
+        int ans  = 0;
+        for(int i=0; i<nums.length; i++){
+            ans ^=nums[i];
+        }
+
+        return ans;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
