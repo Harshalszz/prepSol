e21a2cf0977aa712874744e225b2bc196afa4050
Harshalszz <477harshz@gmail.com>
2026-09-22 23:36:32 +0530

    ValidAnagram Day452

---
+++ DSAProblems/src/ProblemsDay_452/ValidAnagram.java
@@ -0,0 +1,32 @@
+package ProblemsDay_452;
+
+public class ValidAnagram {
+
+
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] character = new int[26];
+
+        for(char ch : s.toCharArray()){
+            character[ch-'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            if(character[ch-'a'] == 0){
+                return false;
+            }
+            character[ch-'a']--;
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
